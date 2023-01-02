187cb1d63e6292cc73f2886f2c3b60c17f2e55e3
iliassemeonidis <paleologos@mail.ru>
2023-01-03 01:45:53 +0300

    урок 5 цвета рисунки картинки фигуры

--- app/src/main/java/com/android/example/startandroidcompose/HomeScreen.kt
+++ app/src/main/java/com/android/example/startandroidcompose/HomeScreen.kt
@@ -1,8 +1,14 @@
 package com.android.example.startandroidcompose
 
 import android.widget.Space
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -11,8 +17,11 @@ import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Alignment.Companion.Start
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -20,100 +29,75 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 
 @Composable
 fun HomeScreen() {
-    Text(
-        text = "Home Screen",
-        fontSize = 32.sp,
-        color = Color.Blue,
-        maxLines = 1,
-        overflow = TextOverflow.Ellipsis,
-        textAlign = TextAlign.Center,
-        modifier = Modifier
-            .background(color = Color.Black)
-            .fillMaxWidth()
-    )
-
-}
-
-@Composable
-fun HomeScreenColum() {
-    // LinearLayout оринтпция вертикальная
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Top,
-    ) {
-        Text(text = "Приветствие", fontSize = 35.sp)
-        Text(
-            text = "Меня зовут Питер Паркер", fontSize = 20.sp,
-            modifier = Modifier.align(Start)
+    Box(contentAlignment = Center, modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .width(160.dp)
+                .height(120.dp)
+                //тут выбираем вигуру
+//                .background(color = Color.Cyan, shape = RoundedCornerShape(16.dp))
+                // тут обрезали углы у фигуры
+//                .background(color = Color.Cyan, shape = CutCornerShape(16.dp))
+                //тут у нас круг
+//                .background(color = Color.Cyan, shape = CircleShape)
+                .background(
+                    brush = Brush.linearGradient(
+                        colors = listOf(
+                            Color.Blue,
+                            Color.Red,
+                            Color.Cyan
+                        )
+                    ),
+                    alpha = 0.2f,
+                    shape = CircleShape
+                )
         )
     }
 }
 
 @Composable
-fun HomeScreenRow() {
-    // LinearLayout оринтпция горизонтальный
-    Row(verticalAlignment = CenterVertically) {
-        Text(text = "Title", fontSize = 25.sp)
-        Spacer(modifier = Modifier.width(8.dp))
-        Text(text = "Description", fontSize = 20.sp)
+fun HomeScreen1() {
+    Box(contentAlignment = Center, modifier = Modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .width(160.dp)
+                .height(120.dp)
+                .background(color = Color.Yellow, shape = RoundedCornerShape(16.dp))
+                .border(
+                    width = 2.dp, brush = Brush.linearGradient(
+                        listOf(
+                            Color.Blue,
+                            Color.Red,
+                            Color.Cyan
+                        )
+                    ), shape = CircleShape
+                )
+        )
     }
 }
 
 @Composable
-fun HomeScreenBox() {
-    //FrameLayout
-    Box {
-        Text(text = "N", fontSize = 45.sp)
-        Text(text = "ame", modifier = Modifier.align(BottomCenter))
-
+fun HomeScreenImage() {
+    Box(contentAlignment = Center, modifier = Modifier.fillMaxSize()) {
+        Image(painter = painterResource(id = R.drawable.hermes), contentDescription = null)
+        Icon(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = null
+        )
     }
 }
 
 @Composable
-fun HomeScreenModif() {
- // тут комбинация лайаутов
-    Row(verticalAlignment = CenterVertically) {
-        HomeScreenBox()
-        Spacer(modifier = Modifier.width(8.dp))
-        Column {
-            Text(text = "Title")
-            Text(text = "Description")
-        }
-    }
-}
-
-@Composable
-fun HomeScreenPadding() {
-    Column(modifier = Modifier.padding(start = 32.dp, end = 16.dp)) {
-        Text(text = "Title")
-        Text(text = "Description")
-
-    }
-}
-
-@Composable
-fun HomeScreenList(list: List<String>) {
-    if (list.isEmpty()) {
-        Box(contentAlignment = Center, modifier = Modifier.fillMaxSize()) {
-            Text(text = "List is empty")
-        }
-    } else {
-        Column {
-            for (s in list) {
-                Text(text = s)
-            }
-        }
-    }
-}
-@Composable
-fun HomeScreenRes() {
-    Column(modifier = Modifier.padding(start = dimensionResource(id = R.dimen.large_padding))) {
-        Text(text = stringResource(id = R.string.home_screen_title))
-        Text(text = stringResource(id = R.string.home_screen_description))
+fun HomeScreenImage2() {
+    Box(contentAlignment = Center, modifier = Modifier.fillMaxSize()) {
+      AsyncImage(
+          model = "https://developer.android.com/images/android-go/next-billion-users_856.png",
+          contentDescription = null
+      )
     }
 }
 
@@ -121,5 +105,5 @@ fun HomeScreenRes() {
 @Preview(showBackground = true)
 @Composable
 fun HomeScreenPreview() {
-    HomeScreenRes()
+    HomeScreenImage2()
 }
