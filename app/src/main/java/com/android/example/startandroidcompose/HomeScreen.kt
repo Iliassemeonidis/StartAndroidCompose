a8cf180ea8eee1220834877f22889621940de605
iliassemeonidis <paleologos@mail.ru>
2023-01-05 02:06:30 +0300

    запилил список

--- app/src/main/java/com/android/example/startandroidcompose/HomeScreen.kt
+++ app/src/main/java/com/android/example/startandroidcompose/HomeScreen.kt
@@ -2,16 +2,17 @@ package com.android.example.startandroidcompose
 
 
 import android.util.Log
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Checkbox
-import androidx.compose.material.OutlinedTextField
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterVertically
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -19,27 +20,49 @@ import androidx.compose.ui.unit.sp
 const val TAG = "TEST_INFO"
 
 @Composable
-fun ClickCounter(
-) {
-    var checked  by remember { mutableStateOf(false) }
-
+fun HomeScreen() {
+    Log.d(TAG, "HomeScreen")
+    val list = remember {
+        List(20) { "Item ${it + 1}" }.toMutableStateList()
+    }
     Column {
-        Row(verticalAlignment = CenterVertically) {
-            Checkbox(checked = checked, onCheckedChange = { checked = it })
-            Text(text = "More details", fontSize = 18.sp)
+        TextButton(onClick = {
+            list.add("Item ${list.size + 1}")
+        }) {
+            Text(text = "Append")
         }
-        Column(modifier = Modifier.padding(15.dp)) {
-            if (checked) {
-                Text(text = stringResource(id = R.string.app_name))
+
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+            contentPadding = PaddingValues(16.dp),
+            modifier = Modifier
+                .border(width = 2.dp, color = Color.Blue)
+        ) {
+            items(list) {
+                SomeItem(text = it)
             }
         }
     }
+
+
+}
+
+@Composable
+fun SomeItem(text: String) {
+    Log.d(TAG, "SomeItem $text")
+    Text(
+        text = text, fontSize = 20.sp,
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(width = 1.dp, color = Color.Black)
+            .padding(16.dp)
+    )
 }
 
 
 @Preview
 @Composable
 fun ClickCounterPreview() {
-    ClickCounter()
+    HomeScreen()
 }
 
