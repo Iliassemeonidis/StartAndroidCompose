4cb37f693f28ca3d25a9269c99d2cdd2e2f5c83a
iliassemeonidis <paleologos@mail.ru>
2023-01-06 23:52:30 +0300

    хороший пример как использовать одну модель для двух экранов

--- app/src/main/java/com/android/example/startandroidcompose/HomeScreen.kt
+++ app/src/main/java/com/android/example/startandroidcompose/HomeScreen.kt
@@ -3,66 +3,65 @@ package com.android.example.startandroidcompose
 
 import android.util.Log
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.toMutableStateList
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 const val TAG = "TEST_INFO"
 
 @Composable
-fun HomeScreen() {
-    Log.d(TAG, "HomeScreen")
-    val list = remember {
-        List(20) { "Item ${it + 1}" }.toMutableStateList()
-    }
+fun HomeScreen(homeViewModel: HomeViewModel = viewModel()) {
+    val uiState by homeViewModel.uiState.collectAsState()
     Column {
-        TextButton(onClick = {
-            list.add("Item ${list.size + 1}")
-        }) {
-            Text(text = "Append")
-        }
-
-        LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(4.dp),
-            contentPadding = PaddingValues(16.dp),
-            modifier = Modifier
-                .border(width = 2.dp, color = Color.Blue)
-        ) {
-            items(list) {
-                SomeItem(text = it)
-            }
-        }
+        ClickCounter(counter = uiState.count, onCounterClick = homeViewModel::onCounterClick)
+        EnableFeature(enabled = uiState.enabled, onCheckedChange = homeViewModel::setEnabled)
     }
-
 
 }
 
 @Composable
-fun SomeItem(text: String) {
-    Log.d(TAG, "SomeItem $text")
+private fun ClickCounter(counter: Int, onCounterClick: () -> Unit) {
     Text(
-        text = text, fontSize = 20.sp,
+        text = "Clicks: $counter",
         modifier = Modifier
-            .fillMaxWidth()
-            .border(width = 1.dp, color = Color.Black)
-            .padding(16.dp)
+            .clickable(onClick = onCounterClick)
     )
 }
 
+@Composable
+fun EnableFeature(
+    enabled: Boolean,
+    onCheckedChange: (Boolean) -> Unit
+) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Checkbox(checked = enabled, onCheckedChange = onCheckedChange)
+        Text("enable feature")
+    }
 
-@Preview
+}
+
+
+@Preview(showBackground = true)
 @Composable
 fun ClickCounterPreview() {
+    ClickCounter(5, onCounterClick = {})
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
     HomeScreen()
 }
 
---
+++ app/src/main/java/com/android/example/startandroidcompose/HomeScreenUiState.kt
@@ -0,0 +1,6 @@
+package com.android.example.startandroidcompose
+
+data class HomeScreenUiState(
+    val count: Int = 0,
+    val enabled: Boolean = false
+)
